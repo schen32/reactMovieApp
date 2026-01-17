12697f19340eb16b2d78a5d84ba7f864c0a2f0bf
Simon Chen <simonc8192@gmail.com>
2026-01-17 06:57:37 -0800

    - import assets

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+        </div>
+      </div>
+    </main>
   )
 }
 
